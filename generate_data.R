2c4b62ac83b20988834ad9bbc2aa716583e0ad6f
Justin Chang <justinchang1124@gmail.com>
2020-12-17 10:41:21 -0500

    deleted old distance parameters, compartmentalized network.layout.animate.circle, added cap_df to more efficiently process incoming vectors

--- generate_data.R
+++ generate_data.R
@@ -68,71 +68,76 @@ make_knn <- function(indices, distances)
     sprintf("I = %s, D = %s", a, b)}, indices, distances) %>% data.frame()
 }
 
+# given a vector of length n x k from RANN, converts it into an n x k data.frame
+# and keeps the first 'cap' columns
+cap_df <- function(vec, n, cap)
+{
+  data.frame(matrix(vec, nrow=n, byrow=TRUE)[,1:cap])
+}
+
+
 # ----------------
 # CREATE ANIMATION
 # ----------------
 
+# given a vector of distances, ensures each entry is at least min_dist
+bound_lower <- function(distances, min_dist)
+{
+  sapply(distances, function(x) {
+    max(min_dist, x)
+  })
+}
+
+# performs normal kamadakawai layout, but with a lower bound on distances
+network_layout_bound <- function(min_dist)
+{
+  function(net, dist.mat = NULL, default.dist = NULL, 
+           seed.coords = NULL, layout.par = list(),verbose=FALSE){
+    default_dists <- layout.distance(net, default.dist = default.dist)
+    
+    network.layout.animate.kamadakawai(
+      net, dist.mat = bound_lower(default_dists, min_dist), default.dist = default.dist, 
+      seed.coords = seed.coords, layout.par = layout.par, verbose = verbose
+    )
+  }
+}
+
 # if instant, makes HTML
 # otherwise, makes an animation htmlWidget for R Shiny
 # min_dist: the minimum distance between graph edges
-make_animation <- function(indices, 
-                           # distances, 
-                           min_dist = 10, instant=TRUE, metadata=NULL)
+make_animation <- function(indices, min_dist = 10, instant=TRUE, metadata=NULL)
 {
-  # if (nrow(indices) != nrow(distances) || ncol(indices) != ncol(distances))
-  # {
-  #   print("Error: Indices and distances are of incorrect dimension.")
-  #   return()
-  # }
-  
   # create the network
   wheel <- network.initialize(nrow(indices))
   
   # colors for metadata
-  met_colors <- "#0000FF"
-  if (!is.null(metadata))
-    met_colors <- make_colors(metadata)
+  met_colors <- make_colors(metadata)
+  if (is.null(metadata))
+  {
+    metadata <- rep("None", nrow(indices))
+    met_colors <- "#00356B" # everyone loves Yale Blue
+  }
   
   # add edges
   for (j in 1:ncol(indices))
-  {
     add.edges.active(
       wheel, 
       tail=1:nrow(indices), 
       head=as.numeric(indices[,j]), 
       onset=j, terminus=ncol(indices)+1,
-      names.eval = rep(list(list("Time"
-                                 # , "Distance"
-                                 )), nrow(indices)),
-      vals.eval = rep(list(list(j
-                                # , distances[,j]
-                                )), nrow(indices))
+      names.eval = list(list("Time")) %>% rep(nrow(indices)),
+      vals.eval = list(list(j)) %>% rep(nrow(indices))
     )
-  }
   
-  # calculate distance layouts, but with the actual distance
-  network.layout.animate.circle <- function(
-    net, dist.mat = NULL,
-    default.dist = NULL, seed.coords = NULL, layout.par = list(),
-    verbose=FALSE){
-    distances <- sapply(
-      layout.distance(net, default.dist = default.dist), function(x) {
-        max(min_dist, x)
-      }
+  # calculate distance layouts, but with a minimum
+  network.layout.animate.circle <- network_layout_bound(min_dist)
+  
+  # compute movement, prior to adding colors / labels to vertices / edges
+  compute.animation(
+    wheel, animation.mode = "circle", slice.par = list(
+      start=0, end=ncol(indices), interval=1, aggregate.dur=1, rule='any'
     )
-    
-    network.layout.animate.kamadakawai(
-      net, dist.mat = distances,
-      default.dist = NULL, seed.coords = NULL, layout.par = list(),
-      verbose=FALSE)
-  }
-  
-  compute.animation(wheel, animation.mode = "circle",
-                    slice.par=list(start=0, end=ncol(indices), 
-                                   interval=1,
-                                   aggregate.dur=1, rule='any'))
-  
-  mode <- ifelse(instant, 'HTML', 'htmlWidget')
+  )
   
   knn <- render.d3movie(
     wheel, 
@@ -142,11 +147,10 @@ make_animation <- function(indices,
     edge.col = make_grey(wheel %e% "Time"),
     vertex.tooltip = paste("<b>Name:</b>", 1:nrow(indices) , "<br>",
                            "<b>Type:</b>", metadata), 
-    # edge.tooltip = paste("<b>Neighbor:</b>", wheel %e% "Time" , "<br>",
-    #                      "<b>Length:</b>", wheel %e% "Distance"), 
-    output.mode=mode
+    output.mode = ifelse(instant, 'HTML', 'htmlWidget')
   )
   
+  # necessary to make the widget resize in R Shiny
   if (!instant)
     knn$sizingPolicy$defaultWidth = "100%"
   
@@ -169,20 +173,16 @@ test <- matrix(testflat, nrow=100)
 write.table(test, "data/test.csv", 
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-i0 <- open_result("data/i07_m100_n0000100_k050_i005.txt")
-i0 <- data.frame(matrix(i0, ncol=50, byrow = TRUE))[,1:10]
-d0 <- open_result("data/d07_m100_n0000100_k050_i005.txt")
-d0 <- sqrt(data.frame(matrix(d0, ncol=50, byrow = TRUE))[,1:10])
+i0 <- open_result("data/i07_m100_n0000100_k050_i005.txt") %>% cap_df(100, 10)
+d0 <- open_result("data/d07_m100_n0000100_k050_i005.txt") %>% cap_df(100, 10)
 
 all_data[[7]][["Raw"]] <- read.csv("data/test.csv", sep=" ", header=FALSE)
 all_data[[7]][["KNN"]] <- make_knn(i0, d0)
-all_data[[7]][["D3M"]] <- make_animation(i0, 10, instant=FALSE)
+all_data[[7]][["D3M"]] <- make_animation(i0, 15, instant=FALSE)
 
 # EXAMPLE 1
-i1 <- open_result("data/i07_m320_n0000344_k010.txt")
-i1 <- data.frame(matrix(i1, nrow=344, byrow=TRUE)[,1:10])
-d1 <- open_result("data/d07_m320_n0000344_k010.txt")
-d1 <- data.frame(matrix(d1, nrow=344, byrow=TRUE)[,1:10])
+i1 <- open_result("data/i07_m320_n0000344_k010.txt") %>% cap_df(344, 10)
+d1 <- open_result("data/d07_m320_n0000344_k010.txt") %>% cap_df(344, 10)
 m1 <- readLines("data/covid_metadata.txt")
 
 all_data[[1]][["Raw"]] <- read.csv("data/time_series_covid_19_confirmed_US_small.csv",
@@ -191,11 +191,8 @@ all_data[[1]][["KNN"]] <- make_knn(i1, d1)
 all_data[[1]][["D3M"]] <- make_animation(i1, 0, instant=FALSE, metadata=m1)
 
 # EXAMPLE 2
-i2 <- open_result("data/i07_m30000_n0000100_k010.txt")
-i2 <- data.frame(matrix(i2, nrow=100, byrow=TRUE)[,1:10])
-d2 <- open_result("data/d07_m30000_n0000100_k010.txt")
-d2 <- data.frame(sqrt(matrix(d2, nrow=100, byrow=TRUE)[,1:10]))
-
+i2 <- open_result("data/i07_m30000_n0000100_k010.txt") %>% cap_df(100, 10)
+d2 <- open_result("data/d07_m30000_n0000100_k010.txt") %>% cap_df(100, 10)
 m2 <- open_result("data/bird_metadata.txt")
 
 all_data[[2]][["Raw"]] <- NULL
@@ -207,18 +204,18 @@ ed1 <- read.csv("data/entex_data.csv")[,-1]
 write.table(hmm, "~/rann_applications/entex_fin.csv", 
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-i3 <- open_result("data/i07_m010_n0000086_k050_i005.txt")
-i3 <- data.frame(matrix(i3, nrow=86, byrow=TRUE)[,1:10])
-d3 <- open_result("data/d07_m010_n0000086_k050_i005.txt")
-d3 <- data.frame(sqrt(matrix(d3, nrow=86, byrow=TRUE)[,1:10]))
-
+i3 <- open_result("data/i07_m010_n0000086_k050_i005.txt") %>% cap_df(86, 10)
+d3 <- open_result("data/d07_m010_n0000086_k050_i005.txt") %>% cap_df(86, 10)
 m3 <- read.csv("data/entex_metadata.csv")$TISSUE
 
 all_data[[3]][["Raw"]] <- ed1
 all_data[[3]][["KNN"]] <- make_knn(i3, d3)
 all_data[[3]][["D3M"]] <- make_animation(i3, 0, instant=FALSE, metadata=m3)
 
-
+# EXAMPLE 4
+# EXAMPLE 5
+# EXAMPLE 6
+# EXAMPLE 7
 
 
 
