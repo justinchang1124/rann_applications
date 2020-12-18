3a6544afe047f183a89f91586570305980b9bc15
Justin Chang <justinchang1124@gmail.com>
2020-12-18 13:38:47 -0500

    final commit

--- generate_data.R
+++ generate_data.R
@@ -58,7 +58,7 @@ make_grey <- function(vec)
 # opens a result from RANN
 open_result <- function(filename)
 {
-  readLines(filename) %>% repStr(" ", "") %>% as.numeric()
+  readLines(filename) %>% repStr(" ", "")
 }
 
 # makes KNN data
@@ -149,6 +149,17 @@ make_animation <- function(indices, min_dist = 10, instant=TRUE, metadata=NULL)
                            "<b>Type:</b>", metadata), 
     output.mode = ifelse(instant, 'HTML', 'htmlWidget')
   )
+  # knn <- render.d3movie(
+  #   wheel, 
+  #   vertex.lwd = 0.5,
+  #   edge.lwd = 1,
+  #   vertex.col = met_colors,
+  #   edge.col = make_grey(wheel %e% "Time"),
+  #   vertex.tooltip = paste("<b>Name:</b>", 1:nrow(indices) , "<br>",
+  #                          "<b>Bird:</b>", metadata[,1], "<br>",
+  #                          "<b>Transformation:</b>", metadata[,2]), 
+  #   output.mode = 'htmlWidget'
+  # )
   
   # necessary to make the widget resize in R Shiny
   if (!instant)
@@ -193,15 +204,17 @@ all_data[[1]][["D3M"]] <- make_animation(i1, 0, instant=FALSE, metadata=m1)
 # EXAMPLE 2
 i2 <- open_result("data/i07_m30000_n0000100_k010.txt") %>% cap_df(100, 10)
 d2 <- open_result("data/d07_m30000_n0000100_k010.txt") %>% cap_df(100, 10)
-m2 <- open_result("data/bird_metadata.txt")
+m2 <- open_result("data/metadata_with_image_types.txt")
+m2 <- strsplit(m2, split="-", fixed=TRUE) %>% unlist() %>% matrix(ncol=2, byrow=TRUE)
 
 all_data[[2]][["Raw"]] <- NULL
 all_data[[2]][["KNN"]] <- make_knn(i2, d2)
-all_data[[2]][["D3M"]] <- make_animation(i2, 0, instant=FALSE, metadata=m2)
+# all_data[[2]][["D3M"]] <- make_animation(i2, 0, instant=FALSE, metadata=m2)
+all_data[[2]][["D3M"]] <- knn
 
 # EXAMPLE 3
 ed1 <- read.csv("data/entex_data.csv")[,-1]
-write.table(hmm, "~/rann_applications/entex_fin.csv", 
+write.table(hmm, "data/entex_fin.csv", 
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 i3 <- open_result("data/i07_m010_n0000086_k050_i005.txt") %>% cap_df(86, 10)
@@ -213,12 +226,43 @@ all_data[[3]][["KNN"]] <- make_knn(i3, d3)
 all_data[[3]][["D3M"]] <- make_animation(i3, 0, instant=FALSE, metadata=m3)
 
 # EXAMPLE 4
-# EXAMPLE 5
-# EXAMPLE 6
+rand_ind <- sample(3000, 1000)
+r4 <- read.csv("data/scRNA.csv",header=FALSE,sep=",")[rand_ind,]
+write.table(r4, "data/scRNA_q.csv", 
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+saveRDS(rand_ind, "data/rand_ind.rds")
+i4 <- open_result("data/i07_m100_n0001000_k020_i005.txt") %>% cap_df(1000, 10)
+d4 <- open_result("data/d07_m100_n0001000_k020_i005.txt") %>% cap_df(1000, 10)
+m4 <- read.csv("data/scRNA_labels.csv", header=FALSE)[,1] %>% as.numeric()
 
+all_data[[4]][["Raw"]] <- r4
+all_data[[4]][["KNN"]] <- make_knn(i4, d4)
+all_data[[4]][["D3M"]] <- make_animation(i4, instant=FALSE, metadata=m4)
 
+# EXAMPLE 5 - Positional
+r5 <- read.csv("data/positional.csv",header=FALSE,sep=",")
+r5[1,1] <- 56.72837707
+r5[,1] <- as.numeric(r5[,1])
 
+i5 <- open_result("data/i07_m010_n0000416_k007_pos.txt") %>% cap_df(416, 7)
+d5 <- open_result("data/d07_m010_n0000416_k007_pos.txt") %>% cap_df(416, 7)
+m5 <- sprintf("Tier %s", read.csv("data/sports_metadata.csv")$Team_Tier)
 
+all_data[[5]][["Raw"]] <- r5
+all_data[[5]][["KNN"]] <- make_knn(i5, d5)
+all_data[[5]][["D3M"]] <- make_animation(i5, instant=FALSE, metadata=m5)
 
+# EXAMPLE 6 - Functional
+r6 <- read.csv("data/functional.csv",header=FALSE,sep=",")
+r6[1,1] <- 59.50225989
+r6[,1] <- as.numeric(r5[,1])
+
+i6 <- open_result("data/i07_m009_n0000416_k007.txt") %>% cap_df(416, 7)
+d6 <- open_result("data/d07_m009_n0000416_k007.txt") %>% cap_df(416, 7)
+m6 <- m5
+
+all_data[[6]][["Raw"]] <- r6
+all_data[[6]][["KNN"]] <- make_knn(i6, d6)
+all_data[[6]][["D3M"]] <- make_animation(i6, instant=FALSE, metadata=m6)
 
 saveRDS(all_data, "rann_app/data/all_data.rds")
